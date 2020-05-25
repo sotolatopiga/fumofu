591311fbc8790261400dd9545206e06a454c62dd
Sotola Topiga <sotolatopiga@gmail.com>
2020-05-25 13:50:07 +0700

    24-May-2020

--- VCSBasics/Program.cs
+++ VCSBasics/Program.cs
@@ -4,11 +4,23 @@ namespace VCSBasics
 {
     class Program
     {
+
+        static int gcd(int a, int b)
+        {
+            while (a != b)
+                if (a > b)
+                    a = a - b;
+                else
+                    b = b - a;
+            return a;
+        }
+        
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("First commit is here!");
             Console.WriteLine("Now, I kinda have a new stuff here");
+            Console.WriteLine(gcd(15, 175));
         }
         
     }
